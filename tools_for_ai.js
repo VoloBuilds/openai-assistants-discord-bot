174fc58e431c49cbeb1e24cd6c6fba4943128d07
ChongDon <chongdon2000@gmail.com>
2024-01-10 01:30:32 -0700

    Added Description of demo functions

--- tools_for_ai.js
+++ tools_for_ai.js
@@ -1,9 +1,35 @@
 const get_weather = (args) => {
     console.log("get_weather was called");
     let location = args.location || "HongKong";
-    let unit = args.unit || "C";
-    return JSON.stringify({ "success": true, "location": location, "temperature": "-30", "unit": unit });
+    let unit = args.unit || "c";
+    return JSON.stringify({ "success": true, "location": location, "temperature": "18", "unit": unit });
 };
+
+/*
+{
+  "name": "get_weather",
+  "description": "Determine weather in my location",
+  "parameters": {
+    "type": "object",
+    "properties": {
+      "location": {
+        "type": "string",
+        "description": "The city and state e.g. San Francisco, CA"
+      },
+      "unit": {
+        "type": "string",
+        "enum": [
+          "c",
+          "f"
+        ]
+      }
+    },
+    "required": [
+      "location"
+    ]
+  }
+}
+*/
 
 
 const get_time = (args) => {
@@ -11,3 +37,16 @@ const get_time = (args) => {
     return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
 }
 module.exports = { get_weather, get_time };
+
+
+/*
+{
+  "name": "get_time",
+  "description": "Get the current GMT time",
+  "parameters": {
+    "type": "object",
+    "properties": {},
+    "required": []
+  }
+}
+*/
